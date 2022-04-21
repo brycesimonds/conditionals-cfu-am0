e971d8255ddaa5b4ee2a3209d0fc4710f3944761
Bryce Simonds <bsimonds14@gmail.com>
2022-04-20 19:13:58 -0600

    Show change in is over 25 variable

--- code_challenges/spicy.rb
+++ code_challenges/spicy.rb
@@ -4,7 +4,7 @@
 #- If the user either has a good record or is over 25 years old, they should pay full price
 #- If the user is not over 25 and has a bad driving record, they need to have someone else sign for the rental
 good_driving_record = false
-is_over_25 = true
+is_over_25 = false
 
 puts "Welcome to Generic Car Rental place!"
 
