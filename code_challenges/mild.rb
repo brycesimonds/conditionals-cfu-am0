a88d660d2b78da1807c933d584aef2eda22addea
Bryce Simonds <bsimonds14@gmail.com>
2022-04-20 19:04:07 -0600

    Show first try at solving mild challenge

--- code_challenges/mild.rb
+++ code_challenges/mild.rb
@@ -2,3 +2,10 @@
 
 # Write a Ruby program that defines a variable that stores an Integer.
 # The program should print out the String "even" if the Integer is even, and the String "odd" if the Integer is odd.
+
+int_variable = 97
+if int_variable == i.even
+  puts "even"
+else
+  puts "odd"
+end
