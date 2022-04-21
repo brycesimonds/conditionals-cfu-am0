a58c68202f7ca55875335114e2d20c97df505ae7
Bryce Simonds <bsimonds14@gmail.com>
2022-04-20 18:37:12 -0600

    Show first attempt of coding the spicy challenge

--- code_challenges/spicy.rb
+++ code_challenges/spicy.rb
@@ -7,6 +7,25 @@ good_driving_record = true
 is_over_25 = true
 ```
 
+good_driving_record = true
+is_over_25 = true
+
+puts "Welcome to Generic Car Rental place!"
+
+If good_driving_record == true && is_over_25 == true
+  puts "Lucky you! You get a discount on the car rental!"
+
+elsif good_driving_record == true || is_over_25 == true
+  puts "I'm exicted to recieve your full payment for the car rental"
+
+elsif good_driving_record == false && is_over_25 == false
+  puts "You're gonna need someone else to sign for this rental. Good try."
+else
+  puts "Error 48tusb: Please google solution."
+end
+
+
+
 Write a program that evaluates the status of the variables above, and uses logic to print one of three possible messages:
 - If the driving record is good and user is over 25 years old, they should get a discount on the car rental
 - If the user either has a good record or is over 25 years old, they should pay full price
